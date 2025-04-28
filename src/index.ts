4df602c06388789a06fc947d99e5dacebb3e362b
Edmund Hung <edmund@cloudflare.com>
2025-04-28 13:26:15 +0100

    kv demo

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,38 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		if (request.method === 'POST') {
+			await env.CACHE.delete('number');
+
+			return Response.redirect(request.url);
+		}
+
+		let number = await env.CACHE.get('number');
+
+		if (number === null) {
+			number = Math.random().toString();
+			await env.CACHE.put('number', number);
+		}
+
+		return new Response(
+			`
+			<!DOCTYPE html>
+			<html>
+				<head>
+				</head>
+				<body>
+					<h1>Random number (cached): ${number}</h1>
+					<form method="post">
+						<button>Delete cache</button>
+					</form>
+				</body>
+			</html>	
+		`,
+			{
+				headers: {
+					'content-type': 'text/html; charset=utf-8',
+				},
+			}
+		);
 	},
 } satisfies ExportedHandler<Env>;
